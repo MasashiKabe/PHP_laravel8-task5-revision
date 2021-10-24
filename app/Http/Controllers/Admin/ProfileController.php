50e3b807e4f944d54b3195c097cd1df3a8d97d2a
kabe <mgmn3019@gmail.com>
2021-10-24 16:21:41 +0000

    PHP_Laravel8課題5

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,5 +7,24 @@ use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
+    //以下課題5応用Actionの追加
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
 }
