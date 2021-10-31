13e3d48e7abbdfd39607ded1673d4c2875bae262
kabe <mgmn3019@gmail.com>
2021-10-31 07:55:14 +0000

    PHP_Laravel11_task

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,17 @@
-//課題4【応用】add Action
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>Myprofile</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'My Profile'を埋め込む --}}
+@section('title', 'My Profile')
+
+{{-- profile.blade.phpの@yield('content')二以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
