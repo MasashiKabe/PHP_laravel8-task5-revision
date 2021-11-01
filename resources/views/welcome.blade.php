820936643ff7084c9fc9af49a34eea46afee8ab3
MasashiKabe <mgmn3019@gmail.com>
2021-11-01 22:13:53 +0900

    修正2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mynews
                 </div>
 
                 <div class="links">
